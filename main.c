1ad703084740de202f6506c529e2ddf8495b54ec
wrinkl3 <al.seward@gmail.com>
2015-01-29 04:09:09 +0400

    yup

--- main.c
+++ main.c
@@ -1,1 +1,14 @@
-test edit
+#include <stdio.h>
+#include <stdlib.h>
+#include "parser.h"
+
+
+int main(int argc, char *argv[]){
+	//check the arg count
+	if(argc != 2){
+		return -1;
+	}
+	//parse the .torrent file
+	parse_meta(argv[1]);
+
+}
